0a4b8083cc48770d4d539b00b4be623fb7bc94b0
Simon Wang <simonw0718@gmail.com>
2019-07-03 16:24:13 +0800

    add check file exist

--- products.py
+++ products.py
@@ -1,12 +1,23 @@
+import os #作業系統 operation system,可以和電腦溝通
+
+products = [] #放在最外面，如果else fail還是可以用
+if os.path.isfile('products.csv'):
+    print('yeah')
+# 如果不在同一資料夾就需要完整路徑,同資料夾就可以相對路徑
+# os模組 的path 模組 的 isfile 功能
+
 #讀取檔案
-products=[]
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #繼續,跳到下一回,並沒有跳出迴圈
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+    with open('products.csv', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue #繼續,跳到下一回,並沒有跳出迴圈
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+
+else:
+	print('找不到檔案...開始新帳本')
+
 
 #讓使用者輸入
 
@@ -23,7 +34,7 @@ print(products[0][0])
 
 for p in products:
 	print(p[0], '的價格是', p[1])
-	
+
 #寫入檔案
 
 with open('products.csv', 'w', encoding='utf-8') as f:
